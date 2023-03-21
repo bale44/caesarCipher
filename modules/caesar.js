ba7252d66e6a32c03c447e7fd46539042bcf0e50
bale44 <114475275+bale44@users.noreply.github.com>
2023-03-21 20:20:04 +0100

    Adding a numbers convert

--- modules/caesar.js
+++ modules/caesar.js
@@ -3,19 +3,22 @@ import { clear } from "./clear.js";
 import { input, result } from "./variables.js";
 
 function convertCaeser(word) {
-  const regex = /^[a-zA-Z]+$/;
+  const regex = /^[a-zA-Z0-9]+$/;
   if (!regex.test(word)) {
     showError();
   } else {
     const wordToLow = word.toLowerCase();
     let newWord = "";
-
     for (let i = 0; i < wordToLow.length; i++) {
-      let convertLetterToNumber = wordToLow[i].charCodeAt() + 13;
-      if (convertLetterToNumber > 122) convertLetterToNumber -= 26;
-      if (word[i] != wordToLow[i]) convertLetterToNumber -= 32;
+      if (!isNaN(wordToLow[i])) {
+        newWord += wordToLow[i];
+      } else {
+        let convertLetterToNumber = wordToLow[i].charCodeAt() + 13;
+        if (convertLetterToNumber > 122) convertLetterToNumber -= 26;
+        if (word[i] != wordToLow[i]) convertLetterToNumber -= 32;
 
-      newWord += String.fromCharCode(convertLetterToNumber);
+        newWord += String.fromCharCode(convertLetterToNumber);
+      }
     }
     clear();
     result.textContent = newWord;
